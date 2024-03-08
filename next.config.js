3717f512a43f787ee6c005704683e2ca88e851c2
dzypherit <31358319+dzypherit@users.noreply.github.com>
2024-03-08 12:45:58 +0800

    updated email forwarder

--- next.config.js
+++ next.config.js
@@ -12,5 +12,9 @@ module.exports = {
     AWS_ACCESS_KEY_ID: process.env.AWS_ACCESS_KEY_ID,
     AWS_SECRET_ACCESS_KEY: process.env.AWS_SECRET_ACCESS_KEY,
     AWS_REGION: process.env.AWS_REGION,
+    SERVICE_PROVIDER: process.env.SERVICE_PROVIDER,
+    USER_EMAIL: process.env.USER_EMAIL,
+    USER_PASS: process.env.USER_PASS,
+    PORT: process.env.PORT,
   },
 };
